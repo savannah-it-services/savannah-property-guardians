df4024b13cfb78eb95e762a5634cd8c2dab5ca99
James Weston <jweston@oleaedge.com>
2026-06-04 16:00:03 -0400

    Rename “Professional Painting” to “Professional Painting & Waterproofing” under “Handyman”

--- src/sitemap.11ty.js
+++ src/sitemap.11ty.js
@@ -22,7 +22,7 @@ module.exports = {
     sitemap += '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n';
 
     pages.forEach((page) => {
-      // Produce clean extensionless URLs without trailing slashes (e.g. /handyman, /handyman-services/gutter-installation-and-repair)
+      // Produce clean extensionless URLs without trailing slashes (e.g. /handyman, /handyman-services/professional-painting-and-waterproofing)
       let cleanPath = page.url
         .replace(/\.html$/, '')
         .replace(/\/$/, '');
