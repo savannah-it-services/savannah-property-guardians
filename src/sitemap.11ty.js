d75a241ac934a1f5b39c14add78592c0d3ea28f9
James Weston <jweston@oleaedge.com>
2026-06-04 16:08:42 -0400

    Rename “Dock and Yard Operations” service to “Exterior Warehouse Maintenance”

--- src/sitemap.11ty.js
+++ src/sitemap.11ty.js
@@ -22,7 +22,7 @@ module.exports = {
     sitemap += '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n';
 
     pages.forEach((page) => {
-      // Produce clean extensionless URLs without trailing slashes (e.g. /handyman, /warehouse-services/rack-and-shelving-installation)
+      // Produce clean extensionless URLs without trailing slashes (e.g. /handyman, /warehouse-services/exterior-warehouse-maintenance)
       let cleanPath = page.url
         .replace(/\.html$/, '')
         .replace(/\/$/, '');
