82d50925e766f9d39efefafc141ca71637170f41
James Weston <jweston@oleaedge.com>
2026-06-08 20:18:08 -0400

    Adding social media icons

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -3,6 +3,16 @@
  * Main JavaScript - All interactive functionality
  */
 
+// Silence the same Chromium DevTools reporter error from client-side code too
+try {
+  Object.defineProperty(window, '__chromium_devtools_metrics_reporter', {
+    configurable: true,
+    enumerable: true,
+    get: function() { return function() {}; },
+    set: function() {}
+  });
+} catch (e) {}
+
 // ============================================
 // Mobile Navigation
 // ============================================
